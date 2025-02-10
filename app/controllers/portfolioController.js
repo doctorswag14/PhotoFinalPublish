8d27f3d02fadae2f611b664187962271035e378c
doctorswag14 <73026185+doctorswag14@users.noreply.github.com>
2025-02-09 19:37:46 -0600

    improved the look of the site on mobile

--- app/controllers/portfolioController.js
+++ app/controllers/portfolioController.js
@@ -13,5 +13,58 @@
                 imageModal.show();
             }
 
+            $scope.brandingImages = [
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding.jpg', alt: 'Corporate Branding 1' },
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding2.jpg', alt: 'Corporate Branding 2' },
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding3.jpg', alt: 'Corporate Branding 3' },
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding4.jpg', alt: 'Corporate Branding 4' },
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding5.jpg', alt: 'Corporate Branding 5' },
+                { src: './app/views/imageFolder/corporatebranding/corporatebranding6.jpg', alt: 'Corporate Branding 6' }
+            ];
+
+            $scope.familyimages = [
+                { src: "./app/views/imageFolder/familes/familes.jpg", alt: "Image 1" },
+                { src: "./app/views/imageFolder/familes/familes2.jpg", alt: "Image 2" },
+                { src: "./app/views/imageFolder/familes/familes3.jpg", alt: "Image 3" },
+                { src: "./app/views/imageFolder/familes/familes4.jpg", alt: "Image 4" },
+                { src: "./app/views/imageFolder/familes/familes5.jpg", alt: "Image 5" },
+                { src: "./app/views/imageFolder/familes/familes6.jpg", alt: "Image 6" },
+                { src: "./app/views/imageFolder/familes/familes7.jpg", alt: "Image 7" },
+                { src: "./app/views/imageFolder/familes/familes8.jpg", alt: "Image 8" },
+                { src: "./app/views/imageFolder/familes/familes9.jpg", alt: "Image 9" },
+                { src: "./app/views/imageFolder/familes/familes10.jpg", alt: "Image 10" },
+                { src: "./app/views/imageFolder/familes/familes11.jpg", alt: "Image 11" },
+                { src: "./app/views/imageFolder/familes/familes12.jpg", alt: "Image 12" },
+                { src: "./app/views/imageFolder/familes/familes13.jpg", alt: "Image 13" },
+                { src: "./app/views/imageFolder/familes/familes14.jpg", alt: "Image 14" },
+                { src: "./app/views/imageFolder/familes/familes15.jpg", alt: "Image 15" },
+                { src: "./app/views/imageFolder/familes/familes16.jpg", alt: "Image 16" },
+                { src: "./app/views/imageFolder/familes/familes17.jpg", alt: "Image 17" },
+                { src: "./app/views/imageFolder/familes/familes18.jpg", alt: "Image 18" },
+                { src: "./app/views/imageFolder/familes/familes19.jpg", alt: "Image 19" },
+                { src: "./app/views/imageFolder/familes/familes20.jpg", alt: "Image 20" },
+                { src: "./app/views/imageFolder/familes/familes21.jpg", alt: "Image 21" }
+            ];
+
+            $scope.seniorimages = [
+                "./app/views/imageFolder/seniors/senior.jpg",
+                "./app/views/imageFolder/seniors/senior2.jpg",
+                "./app/views/imageFolder/seniors/senior3.jpg",
+                "./app/views/imageFolder/seniors/senior4.jpg",
+                "./app/views/imageFolder/seniors/senior5.jpg",
+                "./app/views/imageFolder/seniors/senior6.jpg",
+                "./app/views/imageFolder/seniors/senior7.jpg",
+                "./app/views/imageFolder/seniors/senior8.jpg",
+                "./app/views/imageFolder/seniors/senior9.jpg",
+                "./app/views/imageFolder/seniors/senior10.jpg",
+                "./app/views/imageFolder/seniors/senior11.jpg",
+                "./app/views/imageFolder/seniors/senior12.jpg"
+            ];
+    
+            // $scope.showImage = function (imageSrc) {
+            //     $scope.selectedImage = imageSrc;
+            //     $('#imageModal').modal('show');
+            // };
+
         }]);
 })();
